a33502f4c6370e39c64bc7e31c570f748fe7750f
Constantin Legras <constantin.legras@gmail.com>
2024-02-27 10:17:23 -0600

    Switch to 24V

--- power_controller/Src/output_settings.py
+++ power_controller/Src/output_settings.py
@@ -17,11 +17,11 @@ class OutputSettings(ft.Column):
         self.v3_3_btn = ft.FilledButton(text="3.3 V", expand=True, col={"md": 16, "lg": 8})
         self.v5_btn = ft.FilledButton(text="5 V", expand=True, col={"md": 16, "lg": 8})
         self.v12_btn = ft.FilledButton(text="12 V", expand=True, col={"md": 16, "lg": 8})
-        self.v22_btn = ft.FilledButton(text="22 V", expand=True, col={"md": 16, "lg": 8})
+        self.v22_btn = ft.FilledButton(text="24 V", expand=True, col={"md": 16, "lg": 8})
 
         self.vout_btn_row = ft.ResponsiveRow([self.v3_3_btn, self.v5_btn, self.v12_btn, self.v22_btn], alignment=ft.MainAxisAlignment.CENTER, columns=16)
 
-        self.vout_slider = ft.Slider(min=0, max=22, divisions=221, label="{value} V", round=1)  # 221 = (22 * 10) + 1, for a tick every 0.1 V
+        self.vout_slider = ft.Slider(min=0, max=24, divisions=241, label="{value} V", round=1)  # 221 = (22 * 10) + 1, for a tick every 0.1 V
 
         self.all_controls = [
             ft.Container(height=4),
