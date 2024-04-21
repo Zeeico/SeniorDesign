bc6c941349904777678fe9f7cd667b099d99f5e5
Constantin Legras <constantin.legras@gmail.com>
2024-04-21 15:44:13 -0500

    A lot of clean up of debugging code. Reworked the CAN messages to be easier to control from Python

--- mainboard_code/Core/Src/mymain.cpp
+++ mainboard_code/Core/Src/mymain.cpp
@@ -28,22 +28,14 @@ uint32_t g_BoardDetectTick = 0;	 // Used to periodically check for the presence 
 
 tEmeter emeter0(&hi2c2, eEmeterAddrPins::VS, eEmeterAddrPins::GND, 0);
 tEmeter emeter1(&hi2c2, eEmeterAddrPins::GND, eEmeterAddrPins::GND, 1);
-// tEmeter emeter2(&hi2c1, eEmeterAddrPins::GND, eEmeterAddrPins::GND, 2);
-// tEmeter emeter3(&hi2c1, eEmeterAddrPins::VS, eEmeterAddrPins::GND, 3);
-
-tEmeter emeter3(&hi2c1, eEmeterAddrPins::GND, eEmeterAddrPins::GND, 2);
-tEmeter emeter2(&hi2c1, eEmeterAddrPins::VS, eEmeterAddrPins::GND, 3);
-
+tEmeter emeter2(&hi2c1, eEmeterAddrPins::GND, eEmeterAddrPins::GND, 2);
+tEmeter emeter3(&hi2c1, eEmeterAddrPins::VS, eEmeterAddrPins::GND, 3);
 std::array<tEmeter *, 4> emeterHandlers = {&emeter0, &emeter1, &emeter2, &emeter3};
 
 tMPQ4214 bbController0(&hi2c2, eMPQ4214AddrPins::VLvl4, 0);
 tMPQ4214 bbController1(&hi2c2, eMPQ4214AddrPins::VLvl1, 1);
-// tMPQ4214 bbController2(&hi2c1, eMPQ4214AddrPins::VLvl1, 2);
-// tMPQ4214 bbController3(&hi2c1, eMPQ4214AddrPins::VLvl4, 3);
-
-tMPQ4214 bbController3(&hi2c1, eMPQ4214AddrPins::VLvl1, 2);
-tMPQ4214 bbController2(&hi2c1, eMPQ4214AddrPins::VLvl4, 3);
-
+tMPQ4214 bbController2(&hi2c1, eMPQ4214AddrPins::VLvl1, 2);
+tMPQ4214 bbController3(&hi2c1, eMPQ4214AddrPins::VLvl4, 3);
 std::array<tMPQ4214 *, 4> bbControllerHandlers = {&bbController0, &bbController1, &bbController2, &bbController3};
 
 CAN_TxHeaderTypeDef emeterCanHeader;
@@ -59,16 +51,13 @@ void SendThermistorData();
 void EnableOutput(tMPQ4214 &controller);
 void SetVoltage(tMPQ4214 &controller, uint16_t millivolts);
 
-void I2CClearBus(I2C_HandleTypeDef *instance);
-
 int akash_red_bull_counter = 0;
 
-int command = -1;
-bool controller3en = false;
+std::array<bool, 4> enablePins = {false, false, false, false};
 
 int mymain() {
 	akash_red_bull_counter++;
-	//! Final setup not handled by cubemx generated code
+	// Final setup not handled by cubemx generated code
 	HAL_ADC_Start_DMA(&hadc1, reinterpret_cast<uint32_t *>(thermistorValues.data()), thermistorValues.size());
 
 	akash_red_bull_counter++;
@@ -84,9 +73,11 @@ int mymain() {
 	thermistorCanHeader.DLC = 8;
 	thermistorCanHeader.TransmitGlobalTime = DISABLE;
 
-	// Need to set these high before talking to each BB Controller
-	// HAL_GPIO_WritePin(relay3cmd_GPIO_Port, relay3cmd_Pin, GPIO_PinState::GPIO_PIN_SET);
+	// Need to set these high before talking to each BB Controller, but initialise them low
+	HAL_GPIO_WritePin(relay0cmd_GPIO_Port, relay0cmd_Pin, GPIO_PinState::GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(relay1cmd_GPIO_Port, relay1cmd_Pin, GPIO_PinState::GPIO_PIN_RESET);
 	HAL_GPIO_WritePin(relay2cmd_GPIO_Port, relay2cmd_Pin, GPIO_PinState::GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(relay3cmd_GPIO_Port, relay3cmd_Pin, GPIO_PinState::GPIO_PIN_RESET);
 
 	while (1) {
 		akash_red_bull_counter++;
@@ -97,174 +88,13 @@ int mymain() {
 		}
 
 		if (g_CanTxTick == 0) {
-			// if (!controller3en)
 			for (tEmeter *emeter : emeterHandlers)
 				SendEmeterStatus(*emeter);
 
 			SendThermistorData();
 			g_CanTxTick = cEmeterFeedbackPeriod - 5;  // Account for the delays in the functions
 		}
-
-		if (command == 0) {
-			if (controller3en)
-				InitBBController(bbController3);
-
-			command = -1;
-		}
-
-		else if (command == 1) {
-			SetVoltage(bbController3, 1000);
-			HAL_Delay(3);
-			EnableOutput(bbController3);
-			command = -1;
-		}
-
-		else if (command == 2) {
-			SetVoltage(bbController3, 3300);
-			HAL_Delay(3);
-			EnableOutput(bbController3);
-			command = -1;
-		}
-
-		else if (command == 3) {
-			SetVoltage(bbController3, 5000);
-			HAL_Delay(3);
-			EnableOutput(bbController3);
-			command = -1;
-		}
-
-		else if (command == 16) {
-			// HAL_GPIO_WritePin(relay3cmd_GPIO_Port, relay3cmd_Pin, GPIO_PinState::GPIO_PIN_RESET);
-			HAL_GPIO_WritePin(relay2cmd_GPIO_Port, relay2cmd_Pin, GPIO_PinState::GPIO_PIN_RESET);
-			controller3en = false;
-			command = -1;
-		}
-
-		else if (command == 17) {
-			HAL_GPIO_WritePin(relay2cmd_GPIO_Port, relay2cmd_Pin, GPIO_PinState::GPIO_PIN_SET);
-			controller3en = true;
-			command = -1;
-		}
-
-		else if (command == 18) {
-			I2CClearBus(&hi2c1);
-			command = -1;
-		}
 	}
-}
-
-#define I2C1_SCL_PIN GPIO_PIN_6
-#define I2C1_SCL_PORT GPIOB
-#define I2C1_SDA_PIN GPIO_PIN_7
-#define I2C1_SDA_PORT GPIOB
-void I2CClearBus(I2C_HandleTypeDef *instance) {
-	GPIO_InitTypeDef GPIO_InitStruct;
-	int timeout = 100;
-	int timeout_cnt = 0;
-
-	// 1. Clear PE bit.
-	instance->Instance->CR1 &= ~(0x0001);
-
-	//  2. Configure the SCL and SDA I/Os as General Purpose Output Open-Drain, High level (Write 1 to GPIOx_ODR).
-	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
-	GPIO_InitStruct.Pull = GPIO_PULLUP;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-
-	GPIO_InitStruct.Pin = I2C1_SCL_PIN;
-	HAL_GPIO_Init(I2C1_SCL_PORT, &GPIO_InitStruct);
-	HAL_GPIO_WritePin(I2C1_SCL_PORT, I2C1_SCL_PIN, GPIO_PIN_SET);
-
-	GPIO_InitStruct.Pin = I2C1_SDA_PIN;
-	HAL_GPIO_Init(I2C1_SDA_PORT, &GPIO_InitStruct);
-	HAL_GPIO_WritePin(I2C1_SDA_PORT, I2C1_SDA_PIN, GPIO_PIN_SET);
-
-	// 3. Check SCL and SDA High level in GPIOx_IDR.
-	while (GPIO_PIN_SET != HAL_GPIO_ReadPin(I2C1_SCL_PORT, I2C1_SCL_PIN)) {
-		timeout_cnt++;
-		if (timeout_cnt > timeout)
-			return;
-	}
-
-	while (GPIO_PIN_SET != HAL_GPIO_ReadPin(I2C1_SDA_PORT, I2C1_SDA_PIN)) {
-		// Move clock to release I2C
-		HAL_GPIO_WritePin(I2C1_SCL_PORT, I2C1_SCL_PIN, GPIO_PIN_RESET);
-		asm("nop");
-		HAL_GPIO_WritePin(I2C1_SCL_PORT, I2C1_SCL_PIN, GPIO_PIN_SET);
-
-		timeout_cnt++;
-		if (timeout_cnt > timeout)
-			return;
-	}
-
-	// 4. Configure the SDA I/O as General Purpose Output Open-Drain, Low level (Write 0 to GPIOx_ODR).
-	HAL_GPIO_WritePin(I2C1_SDA_PORT, I2C1_SDA_PIN, GPIO_PIN_RESET);
-
-	//  5. Check SDA Low level in GPIOx_IDR.
-	while (GPIO_PIN_RESET != HAL_GPIO_ReadPin(I2C1_SDA_PORT, I2C1_SDA_PIN)) {
-		timeout_cnt++;
-		if (timeout_cnt > timeout)
-			return;
-	}
-
-	// 6. Configure the SCL I/O as General Purpose Output Open-Drain, Low level (Write 0 to GPIOx_ODR).
-	HAL_GPIO_WritePin(I2C1_SCL_PORT, I2C1_SCL_PIN, GPIO_PIN_RESET);
-
-	//  7. Check SCL Low level in GPIOx_IDR.
-	while (GPIO_PIN_RESET != HAL_GPIO_ReadPin(I2C1_SCL_PORT, I2C1_SCL_PIN)) {
-		timeout_cnt++;
-		if (timeout_cnt > timeout)
-			return;
-	}
-
-	// 8. Configure the SCL I/O as General Purpose Output Open-Drain, High level (Write 1 to GPIOx_ODR).
-	HAL_GPIO_WritePin(I2C1_SCL_PORT, I2C1_SCL_PIN, GPIO_PIN_SET);
-
-	// 9. Check SCL High level in GPIOx_IDR.
-	while (GPIO_PIN_SET != HAL_GPIO_ReadPin(I2C1_SCL_PORT, I2C1_SCL_PIN)) {
-		timeout_cnt++;
-		if (timeout_cnt > timeout)
-			return;
-	}
-
-	// 10. Configure the SDA I/O as General Purpose Output Open-Drain , High level (Write 1 to GPIOx_ODR).
-	HAL_GPIO_WritePin(I2C1_SDA_PORT, I2C1_SDA_PIN, GPIO_PIN_SET);
-
-	// 11. Check SDA High level in GPIOx_IDR.
-	while (GPIO_PIN_SET != HAL_GPIO_ReadPin(I2C1_SDA_PORT, I2C1_SDA_PIN)) {
-		timeout_cnt++;
-		if (timeout_cnt > timeout)
-			return;
-	}
-
-	// 12. Configure the SCL and SDA I/Os as Alternate function Open-Drain.
-	GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
-	GPIO_InitStruct.Pull = GPIO_PULLUP;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-
-	GPIO_InitStruct.Pin = I2C1_SCL_PIN;
-	HAL_GPIO_Init(I2C1_SCL_PORT, &GPIO_InitStruct);
-
-	GPIO_InitStruct.Pin = I2C1_SDA_PIN;
-	HAL_GPIO_Init(I2C1_SDA_PORT, &GPIO_InitStruct);
-
-	HAL_GPIO_WritePin(I2C1_SCL_PORT, I2C1_SCL_PIN, GPIO_PIN_SET);
-	HAL_GPIO_WritePin(I2C1_SDA_PORT, I2C1_SDA_PIN, GPIO_PIN_SET);
-
-	// 13. Set SWRST bit in I2Cx_CR1 register.
-	instance->Instance->CR1 |= 0x8000;
-
-	asm("nop");
-
-	// 14. Clear SWRST bit in I2Cx_CR1 register.
-	instance->Instance->CR1 &= ~0x8000;
-
-	asm("nop");
-
-	// 15. Enable the I2C peripheral by setting the PE bit in I2Cx_CR1 register
-	instance->Instance->CR1 |= 0x0001;
-
-	// Call initialization function.
-	HAL_I2C_Init(instance);
 }
 
 void CheckBoardConnections() {
@@ -274,17 +104,14 @@ void CheckBoardConnections() {
 			if (emeterHandlers[i]->GetInitialised() || bbControllerHandlers[i]->GetInitialised()) {
 				// Board previously connected, so mark the handlers are not initialised
 				emeterHandlers[i]->SetInitialised(false);
-				// bbControllerHandlers[i]->SetInitialised(false);
+				bbControllerHandlers[i]->SetInitialised(false);
 			}
 		}
 		// If board connected
 		else {
-			// Initialise handlers if they aren't yet
+			// Initialise emeter handler, don't touch bb controller handler, let user do that
 			if (!emeterHandlers[i]->GetInitialised())
 				InitEmeter(*(emeterHandlers[i]));
-
-			// if (!bbControllerHandlers[i]->GetInitialised())
-			// 	InitBBController(*(bbControllerHandlers[i]));
 		}
 	}
 }
@@ -303,6 +130,10 @@ void InitEmeter(tEmeter &emeter) {
 }
 
 void InitBBController(tMPQ4214 &controller) {
+	// If the enable pin isn't high, don't try to enable
+	if (!enablePins[controller.GetId()])
+		return;
+
 	MPQ4214VRefLsbReg controllerVRefLsb;
 	MPQ4214VRefMsbReg controllerVRefMsb;
 	controllerVRefLsb.unused = 0b00000;
@@ -385,7 +216,7 @@ void SendEmeterStatus(tEmeter &emeter) {
 void SendThermistorData() {
 	uint8_t txData[8];
 	for (unsigned int i = 0; i < thermistorValues.size(); i++) {
-		if (thermistorValues[i] < std::max(cPowerBoardADCDetectedThreshold, temperatureLUTOffset)) {
+		if (thermistorValues[i] < static_cast<uint32_t>(std::max(cPowerBoardADCDetectedThreshold, temperatureLUTOffset))) {
 			// 0xFFFF indicates invalid data
 			txData[2 * i] = 0xFF;
 			txData[2 * i + 1] = 0xFF;
@@ -435,6 +266,7 @@ void SetVoltage(tMPQ4214 &controller, uint16_t millivolts) {
 	controllerVRefMsb.VREF_H = (scaledMillivolts >> 3) & 0xFF;	// Bits 10-3
 	controller.SetVoltage(&controllerVRefLsb, &controllerVRefMsb);
 	controller.CheckStatus();
+	HAL_Delay(3);  // Delay to allow time for the BB Controller to do its thing, maybe remove later?
 }
 
 // CAN Rx interrupt handler
@@ -444,74 +276,52 @@ void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *p_hcan) {
 	if (HAL_CAN_GetRxMessage(p_hcan, CAN_RX_FIFO0, &rxHeader, rxData) == HAL_OK)
 		g_CanRxTick = 0;
 
-	if (rxData[0] == 0) {
-		command = 0;
-	} else if (rxData[0] == 1) {
-		command = 1;
-	} else if (rxData[0] == 2) {
-		command = 2;
-	} else if (rxData[0] == 3) {
-		command = 3;
-	} else if (rxData[0] == 16) {
-		command = 16;
-	} else if (rxData[0] == 17) {
-		command = 17;
-	} else {
-		command = -1;
-	}
-	return;
-
 	switch (rxHeader.StdId) {
 		// Voltage set
 		case cVoltageSetId: {
-			std::array<uint16_t, 4> voltageSettings;
+			uint16_t millivolts = rxData[1] + (rxData[2] << 8);
 
-			int i = 0;
-			for (uint16_t &vset : voltageSettings) {
-				vset = rxData[i++];
-				vset += (rxData[i++]) << 8;
-			}
+			if (rxData[0] < bbControllerHandlers.size()) {
+				if (millivolts != 0xFFFF) {
+					tMPQ4214 *controller = bbControllerHandlers[rxData[0]];
+					SetVoltage(*controller, millivolts);
+					EnableOutput(*controller);
+				}
 
-			MPQ4214VRefLsbReg VRefLsb;
-			MPQ4214VRefMsbReg VRefMsb;
-
-			if (voltageSettings[0] != cInvalidVoltageCmd) {
-				VRefLsb.unused = 0b00000;
-				VRefLsb.VREF_L = voltageSettings[0] & 0b111;
-				VRefMsb.VREF_H = (voltageSettings[0] >> 3) & 0xFF;
-				bbController0.SetVoltage(&VRefLsb, &VRefMsb);
-			}
-
-			if (voltageSettings[1] != cInvalidVoltageCmd) {
-				VRefLsb.unused = 0b00000;
-				VRefLsb.VREF_L = voltageSettings[1] & 0b111;
-				VRefMsb.VREF_H = (voltageSettings[1] >> 3) & 0xFF;
-				bbController1.SetVoltage(&VRefLsb, &VRefMsb);
-			}
-
-			if (voltageSettings[2] != cInvalidVoltageCmd) {
-				VRefLsb.unused = 0b00000;
-				VRefLsb.VREF_L = voltageSettings[2] & 0b111;
-				VRefMsb.VREF_H = (voltageSettings[2] >> 3) & 0xFF;
-				bbController2.SetVoltage(&VRefLsb, &VRefMsb);
-			}
-
-			if (voltageSettings[3] != cInvalidVoltageCmd) {
-				VRefLsb.unused = 0b00000;
-				VRefLsb.VREF_L = voltageSettings[3] & 0b111;
-				VRefMsb.VREF_H = (voltageSettings[3] >> 3) & 0xFF;
-				bbController3.SetVoltage(&VRefLsb, &VRefMsb);
+				else if (millivolts == 0xFEFE) {
+					tMPQ4214 *controller = bbControllerHandlers[rxData[0]];
+					InitBBController(*controller);
+				}
 			}
 			break;
 		}
 
-		// Current set
 		// Relay set
 		case cRelaySetId:
-			HAL_GPIO_WritePin(relay0cmd_GPIO_Port, relay0cmd_Pin, (GPIO_PinState)(rxData[0] & 0b0001));
-			HAL_GPIO_WritePin(relay1cmd_GPIO_Port, relay1cmd_Pin, (GPIO_PinState)(rxData[0] & 0b0010));
-			HAL_GPIO_WritePin(relay2cmd_GPIO_Port, relay2cmd_Pin, (GPIO_PinState)(rxData[0] & 0b0100));
-			HAL_GPIO_WritePin(relay3cmd_GPIO_Port, relay3cmd_Pin, (GPIO_PinState)(rxData[0] & 0b1000));
+			switch (rxData[0]) {
+				case 0:
+					HAL_GPIO_WritePin(relay0cmd_GPIO_Port, relay0cmd_Pin, (GPIO_PinState)(rxData[1] & 0b1));
+					enablePins[0] = rxData[1] & 0b1;
+					break;
+
+				case 1:
+					HAL_GPIO_WritePin(relay1cmd_GPIO_Port, relay1cmd_Pin, (GPIO_PinState)(rxData[1] & 0b1));
+					enablePins[1] = rxData[1] & 0b1;
+					break;
+
+				case 2:
+					HAL_GPIO_WritePin(relay2cmd_GPIO_Port, relay2cmd_Pin, (GPIO_PinState)(rxData[1] & 0b1));
+					enablePins[2] = rxData[1] & 0b1;
+					break;
+
+				case 3:
+					HAL_GPIO_WritePin(relay3cmd_GPIO_Port, relay3cmd_Pin, (GPIO_PinState)(rxData[1] & 0b1));
+					enablePins[3] = rxData[1] & 0b1;
+					break;
+
+				default:
+					break;
+			}
 
 			break;
 
